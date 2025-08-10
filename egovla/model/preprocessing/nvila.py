0b68ffb37a42e2408c43e8f2dd88aec7f94e4773
Achtoria <2395260357@qq.com>
2025-08-10 14:25:17 +0800

    Remove image tokens in the chat template

--- egovla/model/preprocessing/nvila.py
+++ egovla/model/preprocessing/nvila.py
@@ -50,10 +50,6 @@ class NVILAPreprocessor(BaseVLPreprocessor):
         [ encoded instruction ]
         No per-sample Python loops.
         """
-        # TODO: fit chat template
-        image_prompt = ["<image>\n"] * self.shape_meta["obs"]["rgb"]["horizon"]
-        image_prompt = "".join(image_prompt)
-        instruction = image_prompt + instruction
         messages = [
             {
                 "role": "user",
