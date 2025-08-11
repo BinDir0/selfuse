1cbe5a23c65206c488396b0a3b0304a800d93567
Kevinz-kz <fanlianz1103@gmail.com>
2025-08-11 20:07:43 +0800

    fix some errors

--- egovla/workspace/train_egovla_deepspeed_workspace.py
+++ egovla/workspace/train_egovla_deepspeed_workspace.py
@@ -23,6 +23,7 @@ from egovla.utils.pytorch_util import dict_apply
 from egovla.workspace.base_workspace import BaseWorkspace
 from egovla.policy.egovla import EgoVLA
 from egovla.dataset.base_dataset import BaseImageDataset
+from egovla.dataset.nvila_preprocessor import NVILAPreprocessor
 from egovla.utils.checkpoint_util import TopKCheckpointManager
 from egovla.utils.json_logger import JsonLogger
 from egovla.model.common.lr_scheduler import get_scheduler
@@ -113,6 +114,10 @@ class TrainEgoVLAWorkspace(BaseWorkspace):
         # configure dataset
         dataset: BaseImageDataset
         dataset = hydra.utils.instantiate(cfg.dataset)
+        dataset.set_preprocessor(NVILAPreprocessor(
+            image_preprocessor=self.model.vlm.vlm.get_vision_tower().image_processor,
+            tokenizer=self.model.vlm.vlm.tokenizer
+        ))
         train_dataloader = DataLoader(dataset, collate_fn=dataset.get_collator(), **cfg.dataloader)
 
         # compute normalizer on the main process and save to disk
