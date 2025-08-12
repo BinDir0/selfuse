6a9f00e4c177f79dacc98f11e07ecaef12781d9e
Achtoria <2395260357@qq.com>
2025-08-12 12:20:13 +0800

    Update init for nvila

--- egovla/model/vlm/nvila.py
+++ egovla/model/vlm/nvila.py
@@ -34,7 +34,8 @@ class NVILA(ModuleAttrMixin):
         # Load pre-trained vision-language model
         self.vlm = LlavaLlamaModel.from_pretrained(
             model_name_or_path, 
-            attn_implementation="sdpa"
+            attn_implementation="sdpa", 
+            dtype=torch.bfloat16
         )
         self.tokenizer = self.vlm.tokenizer
         self.vision_tower = self.vlm.get_vision_tower()
--- egovla/workspace/train_egovla_deepspeed_workspace.py
+++ egovla/workspace/train_egovla_deepspeed_workspace.py
@@ -159,7 +159,6 @@ class TrainEgoVLAWorkspace(BaseWorkspace):
         )
 
         # accelerator
-        '''
         dtype_map = {
             'fp16': torch.float16,
             'bf16': torch.bfloat16,
@@ -169,7 +168,6 @@ class TrainEgoVLAWorkspace(BaseWorkspace):
         mixed_precision_dtype = dtype_map.get(accelerator.state.mixed_precision)
         print(f"current mixed precision dtype: {mixed_precision_dtype}")
         self.model = self.model.to(mixed_precision_dtype)
-        '''
         train_dataloader, val_dataloader, self.model, self.optimizer, lr_scheduler = accelerator.prepare(
             train_dataloader, val_dataloader, self.model, self.optimizer, lr_scheduler
         )
