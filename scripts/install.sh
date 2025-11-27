1c34868fb97bcfa15701a83e2d034d615bcc7d2f
Yifan Zhong <zhongyifan001@gmail.com>
2025-11-27 22:30:59 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -39,3 +39,4 @@ pip install -e .
 # install legendvla
 cd ..
 pip install -r requirements.txt
+pip install torch==2.8.0 torchvision==0.23.0 torchaudio==2.8.0 --index-url https://download.pytorch.org/whl/cu128
