c1cca1b73afa623ff2269b296bdcc9d7939d4af6
Yifan Zhong <zhongyifan001@gmail.com>
2026-02-13 13:59:25 +0800

    Update policy server.

--- src/serving/websocket_policy_server.py
+++ src/serving/websocket_policy_server.py
@@ -50,7 +50,7 @@ class RuntimeEngine:
 
         pred_actions = self.policy.post_process(pred_actions.cpu())
         
-        return {"pred_actions": pred_actions.cpu().float().numpy()}
+        return {"pred_actions": pred_actions.cpu().float().numpy()[0]}
 
 
 class EnvWrapper:
