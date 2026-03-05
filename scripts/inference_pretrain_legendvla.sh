b41e81867e4ebfc04067350ae600d618161c155e
Kevinz-kz <fanlianz1103@gmail.com>
2026-03-06 07:24:26 +0800

    modify action head to make current version compatible with older one

--- scripts/inference_pretrain_legendvla.sh
+++ scripts/inference_pretrain_legendvla.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export TOKENIZERS_PARALLELISM=false
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 accelerate launch \
     --multi_gpu \
-    --num_processes 4 \
-    --gpu_ids 4,5,6,7 \
+    --num_processes 8 \
+    --gpu_ids 0,1,2,3,4,5,6,7 \
     --num_machines 1 \
     --mixed_precision bf16 \
     inference_pretrain_legendvla.py
