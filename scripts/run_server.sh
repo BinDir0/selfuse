863a0515ea2c6ebc6bd38df0feab7f9d5a1b55a3
Achtoria <2395260357@qq.com>
2026-03-08 14:44:17 +0800

    feature: support compile cache for policy server

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,2 +1,5 @@
-TORCH_LOGS="+dynamo"
+export TORCH_LOGS="+dynamo"
+# Enable graph cache and CUDA code cache for torch.compile
+export TORCHINDUCTOR_FX_GRAPH_CACHE=1
+export TORCHINDUCTOR_FORCE_CUDA_CODE_CACHE=1
 python -m src.serving.serve_policy
