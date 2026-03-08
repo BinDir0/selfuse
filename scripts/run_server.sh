b885ab18c89c35cf7e25bb3c8b7bf1ef14ae61f1
Achtoria <2395260357@qq.com>
2026-03-08 20:09:22 +0800

    feature: support torch.compile cache

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,5 +1,8 @@
-export TORCH_LOGS="+dynamo"
+TORCH_LOGS=recompiles,guards,cudagraphs
 # Enable graph cache and CUDA code cache for torch.compile
 export TORCHINDUCTOR_FX_GRAPH_CACHE=1
+export TORCHINDUCTOR_AUTOGRAD_CACHE=1
+export TORCHINDUCTOR_CACHE_DIR=$HOME/.cache/torchinductor
+export TRITON_CACHE_DIR=$HOME/.cache/triton
 export TORCHINDUCTOR_FORCE_CUDA_CODE_CACHE=1
 python -m src.serving.serve_policy
