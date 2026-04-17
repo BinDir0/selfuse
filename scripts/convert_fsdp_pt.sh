8fd9dfaa07a14d3d022d21b5c2766f565713a9d0
Kevinz-kz <fanlianz1103@gmail.com>
2026-04-17 03:05:51 +0000

    update fsdp to .pt converting script

--- scripts/convert_fsdp_pt.sh
+++ scripts/convert_fsdp_pt.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 python -m src.utils.convert_fsdp_checkpoint \
-    --checkpoint /efs-exp/zengfanlian/Projects/EgoVLA/outputs/2026.04.06/22.21_legendvla_qwen3_vl_legendvla_qwen3_vl/step_checkpoints/update_step_100000/pytorch_model_fsdp_0  \
-    --output /efs-exp/zengfanlian/Projects/EgoVLA/outputs/2026.04.06/22.21_legendvla_qwen3_vl_legendvla_qwen3_vl/step_checkpoints/update_step_100000/model.pt
+    --checkpoint /share_data/zengfanlian/Projects/EgoVLA/outputs/2026.04.16/03.08_legendvla_qwen3_vl_legendvla_qwen3_vl/checkpoints/update_step=65000
