70597a4d922761484fc7cdb0c8c6dc50cc57a87e
Kevinz-kz <fanlianz1103@gmail.com>
2026-05-04 08:45:15 -0400

    chore(transforms): disable random_resized_crop augmentation

--- src/dataset/data_transforms.py
+++ src/dataset/data_transforms.py
@@ -335,7 +335,7 @@ def process_image(image, depth_image=None, intrinsic=None, aug_transform=None,
         depth_image = depth_image.astype(np.float32) / 1000.0
 
     if aug_transform:
-        image, depth_image, intrinsic = random_resized_crop(image, depth_image, intrinsic)
+        # image, depth_image, intrinsic = random_resized_crop(image, depth_image, intrinsic)
         image = augment_color(image)
         if depth_image is not None:
             depth_image = augment_depth(depth_image)
